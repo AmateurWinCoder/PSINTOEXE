06bfb7be288528b1346fa13bc6896009a28d2a1d
AmateurWinCoder <94489854+AmateurWinCoder@users.noreply.github.com>
2021-11-16 22:49:21 +0100

    Source code and actual programm.

---
+++ psintoexe.ps1
@@ -0,0 +1,19 @@
+﻿Set-PSDebug -off
+Set-ExecutionPolicy Bypass
+
+$source = "$"
+$destination = "$"
+$source = Read-Host "Source path"
+$destination = Read-Host "Destination"
+$confirm = Read-Host "confirm process? (J/N)" 
+if($confirm -eq "j")
+    {
+    Write-Host "Strating Process"
+    Invoke-PS2EXE "$source" "$destination"
+    }
+    else
+    {
+    Write-Host "Stopping Program"
+    pause 3
+    }
+Set-ExecutionPolicy Default
